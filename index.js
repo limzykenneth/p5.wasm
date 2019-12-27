7bf02c8a8853a7ba45f03d6c9a7e1f120462dd3f
limzykenneth <limzy.kenneth@gmail.com>
2019-12-27 21:02:52 +0000

    Refactor implementation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-/*global p5*/
-import(/* webpackChunkName: "index" */ "p5-wasm").then((wasm) => {
-	p5.prototype.wasm = wasm;
+window.p5WasmReady = import(/* webpackChunkName: "index" */ "p5-wasm").then((wasm) => {
+	p5.prototype.wasm = wasm.P5Wasm.new();
+	return Promise.resolve();
+}).catch((e) => {
+	console.log(e);
 });
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,6 +2,7 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 use std::f64::consts::E;
+use crate::utils::set_panic_hook;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,87 +10,85 @@ use std::f64::consts::E;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
+pub struct P5Wasm {
+}
+
 // math/calculation.js
 #[wasm_bindgen]
-pub fn abs(n: f64) -> f64 {
-	n.abs()
-}
+impl P5Wasm {
+	pub fn new() -> P5Wasm {
+		set_panic_hook();
 
-#[wasm_bindgen]
-pub fn ceil(n: f64) -> f64 {
-	n.ceil()
-}
-
-#[wasm_bindgen]
-pub fn constrain(n: f64, low: f64, high: f64) -> f64 {
-	n.min(low).max(high)
-}
-
-#[wasm_bindgen]
-pub fn dist(x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
-	mag(x2 - x1, y2 - y1)
-}
-
-// #[wasm_bindgen]
-// pub fn exp(n: f64) -> f64 {
-// 	n.exp()
-// }
-
-#[wasm_bindgen]
-pub fn floor(n: f64) -> f64 {
-	n.floor()
-}
-
-#[wasm_bindgen]
-pub fn lerp(start: f64, stop: f64, amt: f64) -> f64 {
-	amt * (stop - start) + start
-}
-
-// #[wasm_bindgen]
-// pub fn log(n: f64) -> f64 {
-// 	n.log(E)
-// }
-
-#[wasm_bindgen]
-pub fn mag(x: f64, y: f64) -> f64 {
-	x.hypot(y)
-}
-
-#[wasm_bindgen]
-pub fn map(n: f64, start1: f64, stop1: f64, start2: f64, stop2: f64, within_bounds: JsValue) -> f64 {
-	let mut v: f64 = (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
-	if within_bounds == JsValue::TRUE {
-		if start2 < stop2 {
-			v = constrain(v, start2, stop2);
-		} else {
-			v = constrain(v, stop2, start2);
-		}
+		P5Wasm {}
 	}
 
-	v
-}
+	pub fn abs(&self, n: f64) -> f64{
+		n.abs()
+	}
 
-#[wasm_bindgen]
-pub fn norm(n: f64, start: f64, stop: f64) -> f64 {
-	map(n, start, stop, 0.0, 1.0, JsValue::UNDEFINED)
-}
+	pub fn ceil(&self, n: f64) -> f64 {
+		n.ceil()
+	}
 
-// #[wasm_bindgen]
-// pub fn pow(x: f64, y: f64) -> f64 {
-// 	x.powf(y)
-// }
+	pub fn constrain(&self, n: f64, low: f64, high: f64) -> f64 {
+		n.min(low).max(high)
+	}
 
-#[wasm_bindgen]
-pub fn sq(n: f64) -> f64 {
-	n * n
-}
+	pub fn dist(&self, x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
+		self.mag(x2 - x1, y2 - y1)
+	}
 
-#[wasm_bindgen]
-pub fn sqrt(n: f64) -> f64 {
-	n.sqrt()
-}
+	pub fn exp(&self, n: f64) -> f64 {
+		n.exp()
+	}
 
-#[wasm_bindgen]
-pub fn fract(n: f64) -> f64 {
-	n.fract()
+	pub fn floor(&self, n: f64) -> f64 {
+		n.floor()
+	}
+
+	pub fn lerp(&self, start: f64, stop: f64, amt: f64) -> f64 {
+		amt * (stop - start) + start
+	}
+
+	pub fn log(&self, n: f64) -> f64 {
+		n.log(E)
+	}
+
+	pub fn mag(&self, x: f64, y: f64) -> f64 {
+		x.hypot(y)
+	}
+
+	pub fn map(&self, n: f64, start1: f64, stop1: f64, start2: f64, stop2: f64, within_bounds: JsValue) -> f64 {
+		let mut v: f64 = (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+		if within_bounds == JsValue::TRUE {
+			if start2 < stop2 {
+				v = self.constrain(v, start2, stop2);
+			} else {
+				v = self.constrain(v, stop2, start2);
+			}
+		}
+
+		v
+	}
+
+	pub fn norm(&self, n: f64, start: f64, stop: f64) -> f64 {
+		self.map(n, start, stop, 0.0, 1.0, JsValue::UNDEFINED)
+	}
+
+	pub fn pow(&self, x: f64, y: f64) -> f64 {
+		x.powf(y)
+	}
+
+	pub fn sq(&self, n: f64) -> f64 {
+		n * n
+	}
+
+	pub fn sqrt(&self, n: f64) -> f64 {
+		n.sqrt()
+	}
+
+	pub fn fract(&self, n: f64) -> f64 {
+		n.fract()
+	}
 }
