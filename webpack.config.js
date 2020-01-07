c98aa37651bde3aaac5aeb81fd5cbf46aa4b35c1
limzykenneth <limzy.kenneth@gmail.com>
2020-01-07 11:35:07 +0000

    Prepare for publish on npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require("path");
+const version = require("./package.json").version;
+
+let publicPath = null;
+if(process.env.CDN_BUILD === "true"){
+	publicPath = `https://cdn.jsdelivr.net/npm/p5.wasm@${version}/dist/`;
+}
 
 module.exports = {
 	entry: "./index.js",
@@ -7,7 +13,7 @@ module.exports = {
 		filename: "p5.wasm.js",
 		chunkFilename: "[name].bundle.js",
 		webassemblyModuleFilename: "p5.wasm",
-		publicPath: process.env.ASSETS_PATH || "/wasm/"
+		publicPath: publicPath || process.env.ASSETS_PATH || "/wasm/"
 	},
 	mode: "production"
 };
